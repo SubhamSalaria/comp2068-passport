8d8b184e19780de36fb28af3168d485a5ffa8d0b
SubhamSalaria <200333595@student.georgianc.on.ca>
2017-03-14 15:31:46 -0400

    lab5

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// config.js
+
+// make our details available
+module.exports = {
+  mongodb: 'mongodb://w2017dba:Jaimasatndi786@ds129610.mlab.com:29610/comp2068-w2017',
+  session_secret: 'foobar'
+}
